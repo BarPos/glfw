d331266e888e4a22d21ad5ca1bf88d51a35755c6
Bartłomiej Postek <68648068+BarPos@users.noreply.github.com>
2023-07-18 15:17:24 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -78,16 +78,3 @@ project "GLFW"
 		{
 			"Dwmapi.lib"
 		}
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
